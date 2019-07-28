9d0a07ed16252ddd1da2f843067df3a1d666921c
Nate Murray <nate@natemurray.com>
2019-07-28 11:52:01 -0500

    feat: add reverse

--- __tests__/server.spec.ts
+++ __tests__/server.spec.ts
@@ -18,6 +18,14 @@ describe('server', () => {
     });
   });
 
+      
+  it('reverses', done => {
+    jsonist.get(urlBase + '/reverse/hello', (_, body) => {
+      expect(body.msg).toEqual('olleh');
+      done();
+    });
+  });
+
   afterAll(async () => {
     server.close();
   });
--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,21 @@ function handleUppercase(req: Request, res: Response) {
   res.json({ msg: message.toUpperCase() });
 }
 
+function handleReverse(req: Request, res: Response) {
+  res.json({
+    msg: req.params.msg
+      .split('')
+      .reverse()
+      .join(''),
+  });
+}
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', handleHelloWorld);
 app.get('/uppercase', handleUppercase);
+app.get('/reverse/:msg', handleReverse);
 
 const server = app.listen(app.get('port'), () => {
   console.log('App is running at http://localhost:%d', app.get('port'));
